4e3429a67a608b472ba8c89e7d3fd6031705aeee
ethansaw04 <105765963+ethansaw04@users.noreply.github.com>
2022-07-10 13:03:48 -0700

    Update NPCService.lua
    simplified + added comments

--- src/ServerScriptService/Handlers/NPCService.lua
+++ src/ServerScriptService/Handlers/NPCService.lua
@@ -19,26 +19,26 @@ end)
 function NPCModule.init()
 	while wait(10) do
 		local NewNPC = Template:Clone()
-		local NumberOfAnimals = #ExoticAnimals:GetChildren()
-		local RandomNPC = math.random(1, NumberOfAnimals)
-		for i, v in ipairs(ExoticAnimals:GetChildren()) do
-			if i == RandomNPC then
-				NewNPC.Decal.Texture = require(v).ImageID
-				NewNPC.Parent = game.Workspace
-				NewNPC.Position = Spawner.Position + Vector3.new(math.random(-Spawner.Size.X/2, Spawner.Size.X/2), 2, math.random(-Spawner.Size.Z/2, Spawner.Size.Z/2))
+		
+		local Animals= ExoticAnimals:GetChildren()
+		local RandomNPC = Animals[math.random(1, #Animals)]
+		--create physical NPC for player to interact with and choose from the random exotic animals available
+		
+		NewNPC.Decal.Texture = require(RandomNPC).ImageID
+		NewNPC.Parent = game.Workspace
+		NewNPC.Position = Spawner.Position + Vector3.new(math.random(-Spawner.Size.X/2, Spawner.Size.X/2), 2, math.random(-Spawner.Size.Z/2, Spawner.Size.Z/2))
 
-				NewNPC.Touched:Connect(function(Part)
-					if Part:IsA("BasePart") and Part.Parent then
-						local Player = Players:GetPlayerFromCharacter(Part.Parent)
-						if Player and not Player:GetAttribute("NPCBattle") then
-							Player:SetAttribute("NPCBattle", v.Name)
-							BattleRemotes.NPCBattle:FireClient(Player)
-							NewNPC:Destroy()
-						end
-					end
-				end)
+		NewNPC.Touched:Connect(function(Part)
+			if Part:IsA("BasePart") and Part.Parent then
+				local Player = Players:GetPlayerFromCharacter(Part.Parent)
+				if Player and not Player:GetAttribute("NPCBattle") then
+					Player:SetAttribute("NPCBattle", RandomNPC.Name)
+					BattleRemotes.NPCBattle:FireClient(Player)
+					--send player into battle
+					NewNPC:Destroy()
+				end
 			end
-		end
+		end)
 	end
 end
 
